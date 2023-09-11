312575c5605d8be536024a786639c126d12a32c8
DannielNavas <dannielnavas@gmail.com>
2023-09-11 08:28:04 -0500

    ✨ config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   content: ["./public/index.html", "./src/**/*.{html,js}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
